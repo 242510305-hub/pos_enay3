3d32ac782c071cce3151e8569a116e1c1e33de75
242510305-hub <242510305@smkn4-tsm.sch.id>
2026-08-11 12:03:05 +0700

    Add files via upload

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SearchRequest;
+use App\Http\Requests\User\StoreRequest;
+use App\Http\Requests\User\UpdateRequest;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(SearchRequest $request)
+{
+    $keyword = $request->input('search');
+
+    if ($keyword) {
+        $users = User::whereRaw(
+            "MATCH(name, email) AGAINST(? IN BOOLEAN MODE)", 
+            [$keyword]
+        )
+        ->paginate(10)
+        ->withQueryString();
+    } else {
+        $users = User::query()
+            ->paginate(10)
+            ->withQueryString();
+    }
+        return view('users.index', compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $roles = Role::all();
+
+        return view('users.create', compact('roles'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRequest $request)
+    {
+        $data = $request->validated();
+
+        $data['password'] = Hash::make($data['password']);
+
+        User::create($data);
+
+        return redirect()
+            ->route('admin.users') 
+            ->with('success', 'User berhasil dibuat');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function edit(user $user)
+    {
+         // menerima data user yang akan di edit
+    $roles = Role::all(); // mengambil semua data role
+
+    return view('users.edit', compact('user', 'roles'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRequest $request, User $user)
+    {
+        $dataReq = $request->validated(); // validasi data dari form input
+
+         $user->name = $dataReq['name'];
+         $user->email = $dataReq['email'];
+         $user->role_id = $dataReq['role_id'];
+
+    // jika password diisi maka update password
+    if (!empty($dataReq['password'])) {
+        $user->password = Hash::make($dataReq['password']);
+    }
+
+    $user->save(); 
+
+    return redirect()
+        ->route('admin.users.edit', $user->id)
+        ->with('success', 'User updated');
+    }
+ /**
+  * Remove the specified resource from storage
+  */
+  public function destroy(User $user)
+  {
+    $user->delete();
+
+    return back()->with('success', 'User deleted');
+  }
+ }
